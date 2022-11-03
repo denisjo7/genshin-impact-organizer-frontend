e404863bb534b81582ee3d8cc65cf18932148f24
Denis Jonathan <denisjonathan7@gmail.com>
2022-11-03 19:28:51 -0300

    feat: create util to colors types

---
+++ src/utils/backgroundTypes.js
@@ -0,0 +1,11 @@
+const backgroundTypes = {
+  "Anemo": "bg-[#3fb1b5]",
+  "Cryo": "bg-[#79d0e3]",
+  "Dendro": "bg-[#73a53b]",
+  "Electro": "bg-[#8556b7]",
+  "Geo": "bg-[#be964a]",
+  "Hydro": "bg-[#3f6fb4]",
+  "Pyro": "bg-[#c06544]"
+};
+
+export default backgroundTypes;
