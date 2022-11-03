42d1d14fa3ee30e162e9557a928507da6e203f76
Denis Jonathan <denisjonathan7@gmail.com>
2022-11-02 21:09:57 -0300

    refactor: fix lint errors

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-magic-numbers */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
