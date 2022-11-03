6dd08ac698650f87046cfeaceaca5f9121c3475c
Denis Jonathan <denisjonathan7@gmail.com>
2022-11-03 19:28:26 -0300

    feat: create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="w-full sticky top-0 z-10 py-4 bg-orange-400 text-center">
+      <h1 className="text-3xl font-semibold">
+        Genshin Impact Account Organizer
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
